8f509c354ca56bbdfa1e4fa2d09a529d192d1601
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-03-08 01:59:15 +0000

    almost there

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useRouter } from 'expo-router';
-import {ScrollView, View, Image, Text, TouchableOpacity} from 'react-native';
+import {
+    ScrollView,
+    View,
+    Image,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Button,
+    ActivityIndicator, TextInput
+} from 'react-native';
 import tw from 'twrnc';
 import AegisShield from '../assets/images/Aegis-Shield.png';
 import google from '../assets/images/google.png';
+import {FirebaseError} from "@firebase/util";
+import firebase from "firebase/compat";
+import auth = firebase.auth;
 
 
-const SignIn = () => {
+export default function SignIn  ()  {
     const handleLogin = () => {};
     const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const signUp = async () => {
+        setLoading(true);
+        try {
+            await auth().createUserWithEmailAndPassword(email, password);
+            alert('Check your emails!');
+        } catch (e: any) {
+            const err = e as FirebaseError;
+            alert('Registration failed: ' + err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const signIn = async () => {
+        setLoading(true);
+        try {
+            await auth().signInWithEmailAndPassword(email, password);
+        } catch (e: any) {
+            const err = e as FirebaseError;
+            alert('Sign in failed: ' + err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <ScrollView contentContainerStyle={tw`flex-1 justify-center items-center bg-white px-4`}>
@@ -40,8 +81,37 @@ const SignIn = () => {
                 >
                     Go to home
                 </Text>
-
             </View>
+            <View style={tw`flex-1`} />
+
+            <View style={styles.container}>
+                <KeyboardAvoidingView behavior="padding">
+                    <TextInput
+                        style={styles.input}
+                        value={email}
+                        onChangeText={setEmail}
+                        autoCapitalize="none"
+                        keyboardType="email-address"
+                        placeholder="Email"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        placeholder="Password"
+                    />
+                    {loading ? (
+                        <ActivityIndicator size={'small'} style={{ margin: 28 }} />
+                    ) : (
+                        <>
+                            <Button onPress={signIn} title="Login" />
+                            <Button onPress={signUp} title="Create account" />
+                        </>
+                    )}
+                </KeyboardAvoidingView>
+            </View>
+
             <View style={tw`flex-1`} />
 
             <View style={tw`w-full p-4`}>
@@ -56,5 +126,19 @@ const SignIn = () => {
         </ScrollView>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 20,
+        flex: 1,
+        justifyContent: 'center'
+    },
+    input: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff'
+    }
+});
 
-export default SignIn;
