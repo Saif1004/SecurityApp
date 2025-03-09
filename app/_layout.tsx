38ad28bce199eb6e23995ea639c072c06824c011
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-03-09 17:10:27 +0000

    fixed error! only took 17 hours

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,54 @@
-import { Stack } from "expo-router";
-import {GestureHandlerRootView} from "react-native-gesture-handler";
+import { Stack, useRouter, useSegments } from 'expo-router';
+import { useEffect, useState } from 'react';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
-  return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-         <Stack />
-      </GestureHandlerRootView>
-  );
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+    const router = useRouter();
+    const segments = useSegments();
+
+    const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+        console.log('onAuthStateChanged', user);
+        setUser(user);
+        if (initializing) setInitializing(false);
+    };
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber;
+    }, []);
+
+    useEffect(() => {
+        if (initializing) return;
+
+        const inAuthGroup = segments[0] === 'nav-screens';
+
+        if (user && !inAuthGroup) {
+            router.replace('/nav-screens/HomeScreen');
+        } else if (!user && inAuthGroup) {
+            router.replace('/');
+        }
+    }, [user, initializing]);
+
+    if (initializing)
+        return (
+            <View
+                style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flex: 1
+                }}
+            >
+                <ActivityIndicator size="large" />
+            </View>
+        );
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ title: 'Login' }} />
+            <Stack.Screen name="nav-screens" options={{ headerShown: false }} />
+        </Stack>
+    );
 }
