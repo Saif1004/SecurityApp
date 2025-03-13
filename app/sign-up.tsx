e5fa7757dcbb4538867f7a01fddb3113f5ebaccd
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-03-13 20:57:47 +0000

    adjusted sign in and sign up

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -43,19 +43,7 @@ export default function SignIn  ()  {
                 Aegis Security Systems
             </Text>
 
-            <TouchableOpacity
-                onPress={handleLogin}
-                style={tw`flex-row items-center justify-center bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5`}>
-                <Image source ={google}
-                       style={tw`w-5 h-5 mr-3`}
-                       resizeMode="contain"
-                />
-                <Text style={tw`text-lg font-rubik text-black-300`}>
-                    Continue with Google
-                </Text>
-            </TouchableOpacity>
-
-            <View style={tw`flex-1`} />
+            <View style={tw`flex-0.5`} />
 
             <View style={styles.container}>
                 <KeyboardAvoidingView behavior="padding">
@@ -81,10 +69,22 @@ export default function SignIn  ()  {
                             <Button onPress={signUp} title="Create account" />
                         </>
                     )}
+
+                    <TouchableOpacity
+                        onPress={handleLogin}
+                        style={tw`flex-row items-center justify-center bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5`}>
+                        <Image source ={google}
+                               style={tw`w-5 h-5 mr-3`}
+                               resizeMode="contain"
+                        />
+                        <Text style={tw`text-lg font-rubik text-black-300`}>
+                            Continue with Google
+                        </Text>
+                    </TouchableOpacity>
                 </KeyboardAvoidingView>
             </View>
 
-            <View style={tw`flex-1`} />
+            <View style={tw`flex-1.5`} />
 
             <View style={tw`w-full p-4`}>
                 <Text
