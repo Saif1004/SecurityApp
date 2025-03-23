be1c457e3f3fe907e272a6e141986d87d0f39ae3
Harpreet. <roopraharpreet1@gmail.com>
2025-03-23 22:15:27 +0000

    pls work

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,9 @@ const { getDefaultConfig } = require('@react-native/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
+
 module.exports = config;
