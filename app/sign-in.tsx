48df5a89debfde64c3261113b031ee106d0de46c
Harpreet. <roopraharpreet1@gmail.com>
2025-03-23 23:45:06 +0000

    temp home button

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -37,6 +37,15 @@ export default function SignIn  ()  {
                 style={tw`w-60 h-60`}
                 resizeMode="contain"
             />
+
+            <View style={tw`w-full p-4`}>
+                <Text
+                    onPress={() => router.push('/home')}
+                    style={tw`bg-blue-600 text-white text-center py-3 rounded-lg`}
+                >
+                    home temp button
+                </Text>
+            </View>
             
             <Text style={tw`text-3xl font-bold text-gray-800 mt-4`}>
                 Aegis Security Systems
