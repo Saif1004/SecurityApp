4cc40741acb6be1acf0cc81d3caf6a375297986c
Harpreet. <roopraharpreet1@gmail.com>
2025-03-24 00:48:21 +0000

    lost the spirit

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -44,6 +44,7 @@ export default function SignIn() {
 
             <Text style={tw`text-3xl font-bold text-gray-800 mt-4`}>Aegis Security Systems</Text>
 
+
             <TouchableOpacity
                 onPress={() => {}}
                 style={tw`flex-row items-center justify-center bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5`}
@@ -51,6 +52,15 @@ export default function SignIn() {
                 <Image source={google} style={tw`w-5 h-5 mr-3`} resizeMode="contain" />
                 <Text style={tw`text-lg font-rubik text-black-300`}>Continue with Google</Text>
             </TouchableOpacity>
+
+            <View style={tw`w-full p-4`}>
+                <Text
+                    onPress={() => router.push('/home')}
+                    style={tw`bg-blue-600 text-white text-center py-3 rounded-lg`}
+                >
+                    this is alert
+                </Text>
+            </View>
 
             <View style={tw`w-full mt-5`}>
                 <KeyboardAvoidingView behavior="padding">
