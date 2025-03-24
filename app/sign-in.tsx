58ec8770908278ba46ebb2df307ce57abd4294f1
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-03-24 00:11:38 +0000

    k

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -52,19 +52,7 @@ export default function SignIn  ()  {
             </Text>
 
 
-            <TouchableOpacity
-                onPress={handleLogin}
-                style={tw`flex-row items-center justify-center bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5`}>
-                <Image source ={google}
-                       style={tw`w-5 h-5 mr-3`}
-                       resizeMode="contain"
-                />
-                <Text style={tw`text-lg font-rubik text-black-300`}>
-                    Continue with Google
-                </Text>
-            </TouchableOpacity>
-            <View style={tw`flex-1`} />
-            <View style={tw`flex-0.2`} />
+
 
             <View style={styles.container}>
                 <KeyboardAvoidingView behavior="padding">
