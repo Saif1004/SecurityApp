3ca6cbe9577b289fa08ece8b933c25814b34e17e
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-04-28 15:13:27 +0100

    pin 16

--- flask-server/server.py
+++ flask-server/server.py
@@ -32,7 +32,7 @@ frame_buffer = deque(maxlen=100)
 last_encoded_frame = None
 
 # Solenoid Lock GPIO setup
-LOCK_GPIO_PIN = 25
+LOCK_GPIO_PIN = 16
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LOCK_GPIO_PIN, GPIO.OUT)
